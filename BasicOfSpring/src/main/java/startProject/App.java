879d30f9f7e2214e7fd0c9c2ca00cca079bbb6a1
Yevhenii Zahrebelnyii <zahreb.yevhen@gmail.com>
2020-11-30 15:27:23 +0200

    Basic 3
    - created spring.xml
    - created EventLogger
    - learning dependency injection

--- BasicOfSpring/src/main/java/startProject/App.java
+++ BasicOfSpring/src/main/java/startProject/App.java
@@ -1,18 +1,26 @@
 package startProject;
 
 import entity.Client;
-import logger.ConsoleEventLogger;
+import logger.EventLogger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     private Client client;
-    private ConsoleEventLogger eventLogger;
+    private EventLogger eventLogger;
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
 
     public static void main(String[] args) {
-        App app = new App();
-        app.client = new Client(1L, "John Smith");
-        app.eventLogger = new ConsoleEventLogger();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+
+        App app = (App) applicationContext.getBean("app");
 
         app.logEvent("Some event for user 1");
+        app.logEvent("Some event for user 2");
     }
 
     private void logEvent(String msg) {
